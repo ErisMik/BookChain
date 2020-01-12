ac963ebb73201cccb9faf8b987cc335d70509be8
Eric Mikulin <ericm99@gmail.com>
2020-01-11 17:39:20 -0800

    Add networking app

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # BookChain
+
+## Backend
+To build: `docker build . -t bookchain`
+To run: `docker run -p 5000:5000 bookchain`
+Navigate to `localhost:5000`
---
+++ src/app.py
@@ -0,0 +1,50 @@
+import requests
+from flask import Flask, jsonify
+from multiprocessing import Process, Pipe
+from netaddr import IPNetwork, IPAddress
+
+from storage import readChain
+
+LINK_CODE = 'Bookchain Linky'
+
+app = Flask(__name__)
+peers = []
+
+@app.route('/')
+def pong():
+    return LINK_CODE
+
+@app.route('/findpeers', methods=['GET'])
+def find_peers():
+    all_ips = list(IPNetwork("192.168.0.0/24"))
+    for ip in all_ips:
+        try:
+            print(f"Trying: {str(ip)}")
+            r = requests.get(f"http://{str(ip)}:5000/", timeout=1)
+
+            if LINK_CODE in r.text:
+                peers.append(str(ip))
+        except:
+            pass
+
+@app.route('/blocks', methods=['GET'])
+def blocks():
+    blockchain = []
+    for block in readChain():
+        blockchain.append(block)
+
+    return jsonify(blockchain)
+
+if __name__ == '__main__':
+    print("======================")
+    print("Bookchain starting up!")
+    print("======================")
+
+    # Start mining
+    a, b = Pipe()
+    # p1 = Process(target=mine, args=(a, BLOCKCHAIN, NODE_PENDING_TRANSACTIONS))
+    # p1.start()
+
+    # Start server to receive transactions
+    p2 = Process(target=app.run(host="0.0.0.0", port="5000"), args=b)
+    p2.start()

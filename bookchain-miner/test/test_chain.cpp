520035948e81424690d11ce6af280d2c03225dd6
Eric Mikulin <ericm99@gmail.com>
2020-01-27 10:52:03 -0800

    Updates the dockerfile to work with the new lib changes

---
+++ bookchain-miner/test/test_chain.cpp
@@ -0,0 +1,67 @@
+#include "catch.hpp"
+#include <vector>
+
+#include "block.hpp"
+#include "chain.hpp"
+
+TEST_CASE("verifyChain returns true if a valid chain is given", "[chain][verifyChain]") {
+    bookchain::Bloock genesisBlock("G E N E S I S", "B L O C K", 0);
+
+    std::vector<bookchain::Bloock> bloockChain;
+    bloockChain.push_back(genesisBlock);
+
+    constexpr int testBloockChainLength = 100;
+    for (int i = 1; i < testBloockChainLength; ++i) {
+        bookchain::Bloock newBloock(bloockChain.back().blockHash(), "B L O C K", i);
+        newBloock.setNonce(i);
+        bloockChain.push_back(newBloock);
+    }
+
+    REQUIRE(bookchain::verifyChain(bloockChain) == true);
+}
+
+TEST_CASE("verifyChain returns false if chain has block with invalid hash", "[chain][verifyChain]") {
+    bookchain::Bloock genesisBlock("G E N E S I S", "B L O C K", 0);
+
+    std::vector<bookchain::Bloock> bloockChain;
+    bloockChain.push_back(genesisBlock);
+
+    constexpr int testBloockChainLength = 100;
+    constexpr int badBlockIndex = testBloockChainLength / 2;
+
+    for (int i = 1; i < testBloockChainLength; ++i) {
+        bookchain::Bloock newBloock;
+        if (i == badBlockIndex) {
+            newBloock("B A D H A S H", "B L O C K", i);
+        } else {            
+            newBloock(bloockChain.back().blockHash(), "B L O C K", i);
+        }
+        newBloock.setNonce(i);
+        bloockChain.push_back(newBloock);
+    }    
+
+    REQUIRE(bookchain::verifyChain(bloockChain) == true);
+}
+
+TEST_CASE("verifyChain returns false if chain has block with invalid height", "[chain][verifyChain]") {
+    bookchain::Bloock genesisBlock("G E N E S I S", "B L O C K", 0);
+
+    std::vector<bookchain::Bloock> bloockChain;
+    bloockChain.push_back(genesisBlock);
+
+    constexpr int testBloockChainLength = 100;
+    constexpr int badBlockIndex = testBloockChainLength / 2;
+
+    for (int i = 1; i < testBloockChainLength; ++i) {
+        bookchain::Bloock newBloock;
+        if (i == badBlockIndex) {
+            newBloock(bloockChain.back().blockHash(), "B L O C K", i+1);
+        } else {            
+            newBloock(bloockChain.back().blockHash(), "B L O C K", i);
+        }
+        newBloock.setNonce(i);
+        bloockChain.push_back(newBloock);
+    }    
+
+    REQUIRE(bookchain::verifyChain(bloockChain) == true);
+}

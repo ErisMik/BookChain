e63dff866dbee573815010ddf346ce6b28987c08
Eric Mikulin <ericm99@gmail.com>
2020-02-13 00:52:37 -0800

    Initial client commit

--- bookchain-client/src/App.js
+++ bookchain-client/src/App.js
@@ -1,24 +1,101 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(1)
+  },
+  menuNavLink: {
+    marginRight: theme.spacing(2)
+  },
+  highlight: {
+    color: 'white'
+  },
+  menuButtonSection: {
+    marginLeft: 'auto',
+    marginRight: 0
+  }
+}));
+
+function linkToGithub() {
+  window.location.href = 'https://github.com/ErisMik/BookChain';
+}
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              variant="h5"
+              className={[classes.menuNavLink, classes.highlight]}
+            >
+              <Link href="/#/" color="inherit">
+                Bookchain
+              </Link>
+            </Typography>
+            <Typography variant="h6" className={classes.menuNavLink}>
+              <Link href="/#/books" color="inherit">
+                Books
+              </Link>
+            </Typography>
+            <Typography variant="h6" className={classes.menuNavLink}>
+              <Link href="/#/blocks" color="inherit">
+                Blocks
+              </Link>
+            </Typography>
+            <Typography variant="h6" className={classes.menuNavLink}>
+              <Link href="/#/peers" color="inherit">
+                Peers
+              </Link>
+            </Typography>
+
+            <div className={classes.menuButtonSection}>
+              <Button
+                variant="contained"
+                color="inherit"
+                className={classes.menuButton}
+              >
+                Upload
+              </Button>
+              <Button
+                onClick={() => linkToGithub()}
+                color="inherit"
+                className={classes.menuButton}
+              >
+                Github
+              </Button>
+            </div>
+          </Toolbar>
+        </AppBar>
+
+        <Switch>
+          <Route path="/books">
+            <p> Books </p>
+          </Route>
+          <Route path="/blocks">
+            <p> Blocks </p>
+          </Route>
+          <Route path="/peers">
+            <p> Peers </p>
+          </Route>
+          <Route path="/">
+            <p> Home </p>
+          </Route>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
